258eba3d85ba9e5f12edce8a944159e8c0b3449e
Akshay-Anand-Code <akshay1079410@gmail.com>
2025-06-22 14:56:41 +0530

    nigga

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  title: 'Nigass in Paris',
+  title: 'Nigga in Paris',
   description: 'Nigga in Paris',
   icons: {
     icon: '/favicon.ico',
